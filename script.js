46b85f6785b94272c312b9f0dc6103542c367940
Its Hashmii <152167064+mian-owais@users.noreply.github.com>
2025-02-16 16:42:23 +0500

    Real_Time Character Counter
    There is one thing That it counts space as well as a character.

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-const button = document.querySelectorAll('.box')
-const body= document.querySelector("body")
+const textArea = document.getElementById("Text_Area")
+const totalChar = document.getElementById("Total_Char")
+const remainChar = document.getElementById("Remaining_Char")
 
 
-button.forEach(function(box){
-    box.addEventListener('click', function(color){
-        if(color.target.id === "box1"){
-            body.style.backgroundColor="#85A98F";
-        }
-        if(color.target.id === "box2"){
-            body.style.backgroundColor= "#FCF596";
-        }
-        if(color.target.id === "box3"){
-            body.style.backgroundColor="#48A6A7";
-        }
-        if(color.target.id === "box4"){
-            body.style.backgroundColor="#FFBF61";
-        }
-        if(color.target.id === "box5"){
-            body.style.backgroundColor="#F0C1E1";
-        }
-    })
+textArea.addEventListener("keyup", () => {
+    updateCounter()
 })
+
+updateCounter()
+
+function updateCounter(){
+    totalChar.innerText = textArea.value.length
+    remainChar.innerText =  textArea.getAttribute("maxLength") - textArea.value.length
+}
